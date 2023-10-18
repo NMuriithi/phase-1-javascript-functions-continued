f3f8daa3abfdb610c9df58061746aa1f15a94872
Nicholus <nicholus.muriithi@student.moringaschool.com>
2023-10-18 02:45:31 -0700

    Tricky solution on third function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return `Today is Saturday, I want to ${activity}!`;
+}
+function mondayWork (activity = 'go to the office') {
+    return `Today is Monday, I want to ${activity}!`;
+}
+
+function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+   }
+  
